117676b03d078777cf4cbaedabff8c32a0d70e92
Rimuru-Gio <giojako2020.g@gmail.com>
2024-11-05 19:11:12 +0400

    second commit

---
+++ Day 06/HomeWork/Homework.py
@@ -0,0 +1,9 @@
+name="giorgi"
+name="Saba"
+name="mate"
+name="luka"
+name="sandro"
+print(name)
+
+z=15
+print(5+5)
---
+++ Day 08/HomeWork/homework.py
@@ -0,0 +1,12 @@
+num1=15
+num2=62
+num3=64
+num4=965
+num5=8532
+num6=41
+print((num1 + num2) % num4 - (num5 * num3) / num6)
+
+name=input("Enter Your Name :")
+surname=input("Enter Your Surname :")
+print(type(name))
+print(type(surname))
