74aa3a578e6830db038742a9945ccdf38fd6aff9
giojako2020.g@gmail.com <qweqwe@gmail.com>
2025-03-09 19:23:06 +0400

    commit

---
+++ Day75/ClassWork/cw.js
@@ -0,0 +1,19 @@
+const myInfo = {
+    firstName: "Giorgi",
+    lastName: "Gloveli",
+    age: 2,
+    hobbies: ["Gym", "coding"]
+};
+
+const firstName = myInfo.firstName;
+const lastName = myInfo.lastName;
+const age = myInfo.age;
+const hobbies = myInfo.hobbies;
+myInfo["My Info"] = {
+    firstName,
+    lastName,
+    age,
+    hobbies
+}
+
+console.log(myInfo);
